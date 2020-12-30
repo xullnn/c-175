22de69c46ac245cfb76ee6a764cb0d2424ec15a5
gitCavendish <xll290016537@gmail.com>
2020-12-30 15:50:59 +0800

    starting networked applications with Javascript

---
+++ codes/dice_rolling_app/app.js
@@ -0,0 +1,21 @@
+const HTTP = require('http'),
+      PORT = 3000;
+
+const SERVER = HTTP.createServer((req, res) => {
+  let path = req.url,
+      method = req.method;
+
+  if (path === '/favicon.ico') {
+    res.statusCode = 404;
+    res.end();
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write(`${method} ${path}\n`);
+    res.end();
+  }
+});
+
+SERVER.listen(PORT, () => {  // start listening
+  console.log(`Server listening on port ${PORT}...`);
+});
